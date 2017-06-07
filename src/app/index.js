63985f1e826631dee3aab16f56da9c139eb6cefb
Karol Syta <krallxz@gmail.com>
2017-06-07 20:37:01 +0200

    Added BrowserRouter just for test.

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                Just a first test.
+            </div>
+        )
+    }
+}
+
+class Contact extends React.Component {
+    render() {
+        return(
+            <div>Contact should be there.</div>
+        )
+    }
+}
+
+class Error404 extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Error 404</h1>
+                <p>Not Found</p>
+            </div>
+        )
+    }
+}
 
 class App extends React.Component {
-    render(){
+    render() {
         return(
-            <h1>Karol Syta</h1>
+            <BrowserRouter>
+                <div>
+                    <h1>Karol Syta</h1>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/contact" component={Contact} />
+                        <Route component={Error404} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         )
     }
 }
